b38ee2b78b6710692ba883fe857124d25c3b6fa9
phasezero <phasezero@gmx.net>
2016-12-04 10:05:32 +0100

    Signed-off-by: phasezero <phasezero@gmx.net>
    Still building up django framework
    Started 1st app called "temperatures"
    TODO:
    1. model
    2. views
    3. templates

--- temperatures/urls.py
+++ temperatures/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from . import views
+
+app_name = 'temperatures'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^temperatures/' temperatures.site.urls),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
